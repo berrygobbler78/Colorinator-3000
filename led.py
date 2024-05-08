aa68dd26f5e7646a3a5043998a8809be4128a2c4
berrygobbler78 <127357762+berrygobbler78@users.noreply.github.com>
2024-05-08 10:20:19 -0700

    Add files via upload

---
+++ led.py
@@ -0,0 +1,26 @@
+import time
+import board
+import digitalio
+import adafruit_rgbled
+import adafruit_character_lcd.character_lcd as characterlcd
+
+lcd_rs = digitalio.DigitalInOut(board.D2)
+lcd_en = digitalio.DigitalInOut(board.D3)
+lcd_d7 = digitalio.DigitalInOut(board.D26)
+lcd_d6 = digitalio.DigitalInOut(board.D19)
+lcd_d5 = digitalio.DigitalInOut(board.D13)
+lcd_d4 = digitalio.DigitalInOut(board.D6)
+
+lcd_columns = 16
+lcd_rows = 2
+
+lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
+
+red_pin = board.D16
+green_pin = board.D20
+blue_pin = board.D21
+
+
+led = adafruit_rgbled.RGBLED(red_pin, blue_pin, green_pin)
+while True:
+    led.color = (187, 0, 255)
