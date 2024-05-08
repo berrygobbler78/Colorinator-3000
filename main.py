36573ce027038e0f3e53f924477fa6ceb51efb9e
berrygobbler78 <127357762+berrygobbler78@users.noreply.github.com>
2024-05-08 11:16:57 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -16,20 +16,26 @@ lcd_rows = 2
 
 lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
 
-red_pin = board.D16
-green_pin = board.D20
-blue_pin = board.D21
+red = board.D16
+green = board.D20
+blue = board.D21
+
+led = adafruit_rgbled.RGBLED(red, blue, green)
+
+
+btn = digitalio.DigitalInOut(board.D14)
+btn.direction = digitalio.Direction.INPUT
+btn.pull = digitalio.Pull.UP
+
+prev_state = btn.value
 
 while True:
-    lcd.message = "Hold button to select color"
     cur_state = btn.value
     if cur_state != prev_state:
         if not cur_state:
-            lcd.message = "btn"
+            lcd.message = "button down"
+            led.color = (0, 255, 0)
         else:
-            lcd.message = "btn up"
-
-
-# led = adafruit_rgbled.RGBLED(red_pin, blue_pin, green_pin)
-# while True:
-#     led.color = (187, 0, 255)
+            lcd.message = "button up"
+            led.color = (255, 0, 0)
+    prev_state = cur_state
