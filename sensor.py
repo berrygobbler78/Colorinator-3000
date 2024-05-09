1f593c19851eeea63e75f313d68314902feaafb7
berrygobbler78 <william.garis.bhs@gmail.com>
2024-05-08 18:23:31 -0700

    retry

--- sensor.py
+++ sensor.py
@@ -3,32 +3,27 @@ import board
 import pulseio
 
 # Configure the sensor pins
-s0 = pulseio.PWMOut(board.D12, frequency=1000, duty_cycle=2**15)
-s1 = pulseio.PWMOut(board.D7, frequency=1000, duty_cycle=2**15)
-s2 = pulseio.PWMOut(board.D8, frequency=1000, duty_cycle=2**15)
-s3 = pulseio.PWMOut(board.D25, frequency=1000, duty_cycle=2**15)
-sensor_out = pulseio.PWMOut(board.D24, frequency=1000, duty_cycle=2**15)
+s2 = pulseio.DigitalInOut(board.D2)
+s3 = pulseio.DigitalInOut(board.D3)
+sensor_out = pulseio.DigitalInOut(board.D4)
 
 # Function to read color
 def read_color():
-    # Set frequency scaling to 2%
-    s0.duty_cycle = 6554
-    s1.duty_cycle = 6554
     # Red
-    s2.duty_cycle = 0
-    s3.duty_cycle = 0
+    s2.value = False
+    s3.value = False
     time.sleep(0.1)
-    red = sensor_out.duty_cycle
+    red = sensor_out.value
     # Green
-    s2.duty_cycle = 6554
-    s3.duty_cycle = 0
+    s2.value = True
+    s3.value = False
     time.sleep(0.1)
-    green = sensor_out.duty_cycle
+    green = sensor_out.value
     # Blue
-    s2.duty_cycle = 0
-    s3.duty_cycle = 6554
+    s2.value = False
+    s3.value = True
     time.sleep(0.1)
-    blue = sensor_out.duty_cycle
+    blue = sensor_out.value
     return red, green, blue
 
 # Main loop
