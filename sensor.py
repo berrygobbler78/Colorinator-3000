43fb7df9a92628e83e961230f875eaf3ec623752
berrygobbler78 <william.garis.bhs@gmail.com>
2024-05-08 19:39:11 -0700

    fix

--- sensor.py
+++ sensor.py
@@ -2,7 +2,6 @@ import RPi.GPIO as GPIO
 import time
 import board
 import digitalio
-import adafruit_rgbled
 import adafruit_character_lcd.character_lcd as characterlcd
 
 GPIO.cleanup()
@@ -12,36 +11,42 @@ s3 = 25
 signal = 24
 NUM_CYCLES = 10
 
-lcd_rs = digitalio.DigitalInOut(board.D2)
-lcd_en = digitalio.DigitalInOut(board.D3)
-lcd_d7 = digitalio.DigitalInOut(board.D26)
-lcd_d6 = digitalio.DigitalInOut(board.D19)
-lcd_d5 = digitalio.DigitalInOut(board.D13)
-lcd_d4 = digitalio.DigitalInOut(board.D6)
-
-lcd_columns = 16
-lcd_rows = 2
-
-lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
-
-red_pin = board.D16
-green_pin = board.D20
-blue_pin = board.D21
-
-
-led = adafruit_rgbled.RGBLED(red_pin, blue_pin, green_pin)
+redPin = 16
+greenPin = 20
+bluePin = 21
+#set pins as outputs
 
 GPIO.cleanup()
 
-def setup():
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(signal,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-  GPIO.setup(s2,GPIO.OUT)
-  GPIO.setup(s3,GPIO.OUT)
-  print("\n")
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(signal,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(s2,GPIO.OUT)
+GPIO.setup(s3,GPIO.OUT)
+
+GPIO.setup(redPin,GPIO.OUT)
+GPIO.setup(greenPin,GPIO.OUT)
+GPIO.setup(bluePin,GPIO.OUT)
+
+GPIO.output(redPin, 0)
+GPIO.output(greenPin, 0)
+GPIO.output(bluePin, 0)
+
+RED = GPIO.PWM(redPin, 100)
+GREEN = GPIO.PWM(greenPin, 100)
+BLUE = GPIO.PWM(bluePin, 100)
+RED.start(0)
+GREEN.start(0)
+BLUE.start(0)
+
+print("\n")
   
 
-
+def setColor(rgb = []):
+    # Convert 0-255 range to 0-100.
+    rgb = [(x / 255.0) * 100 for x in rgb]
+    RED.ChangeDutyCycle(rgb[0])
+    GREEN.ChangeDutyCycle(rgb[1])
+    BLUE.ChangeDutyCycle(rgb[2])
 
 
 def loop():
@@ -79,15 +84,14 @@ def loop():
     print("green value - ",green)
     time.sleep(2)  
 
-    led.color = (red, green, blue)
+    setColor([int(red), int(green), int(blue)])
+
 
 
 def endprogram():
     GPIO.cleanup()
 
 if __name__=='__main__':
-    
-    setup()
 
     try:
         loop()
